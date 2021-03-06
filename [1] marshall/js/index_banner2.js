5dc3296760ac885ccc6f9eeedd45e3dfa228d829
dahye-kim <rlaekgp94@gmail.com>
2021-03-06 23:27:46 +0900

    marshall up

--- [1] marshall/js/index_banner2.js
+++ [1] marshall/js/index_banner2.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 		 		
 
-    var swiper = new Swiper('.swiper-container', {
-      autoplay:{
-          delay:5000,
-          disableOnInteraction: false,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-    
+//    var swiper = new Swiper('.swiper-container', {
+//      autoplay:{
+//          delay:5000,
+//          disableOnInteraction: false,
+//      },
+//      navigation: {
+//        nextEl: '.swiper-button-next',
+//        prevEl: '.swiper-button-prev',
+//      },
+//    });
+//    
     
 });
 
