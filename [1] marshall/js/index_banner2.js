61c035cfc49aa72302919ed60c42a025328effd1
dahye-kim <rlaekgp94@gmail.com>
2021-03-20 09:11:37 +0900

    marshall last

--- [1] marshall/js/index_banner2.js
+++ [1] marshall/js/index_banner2.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
-		 		
+	
+    var myArray = ["01","01","02","03"];
+
 
     var swiper = new Swiper('.swiper-container', {
         loop:true,
       effect: 'fade',
-//      autoplay:{
-//          delay:5000,
-//          disableOnInteraction: false,
-//      },
-      pagination: {
+      autoplay:{
+          delay:6000,
+          disableOnInteraction: false,
+      },    
+        pagination: {
         el: '.swiper-pagination',
-        type: 'progressbar',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + myArray[index + 1] + '</span>';
+        },
       },
     });
     
