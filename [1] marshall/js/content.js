eb1607c3b31187ea896145cc47ca60f8c9c3b228
dahye-kim <rlaekgp94@gmail.com>
2021-03-23 13:06:05 +0900

    content02

--- [1] marshall/js/content.js
+++ [1] marshall/js/content.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     var swiper = new Swiper('.swiper-section02-banner', {
         loop: true,
         effect: 'fade',
-        autoplay: {
-            delay: 7000,
-            disableOnInteraction: false,
-        },
+//        autoplay: {
+//            delay: 7000,
+//            disableOnInteraction: false,
+//        },
         pagination: {
         el: '.swiper-pagination',
         type: 'progressbar',
