bde9a6cf7181f9b0e8f8326bd08a9b7a8faa9aec
dahye-kim <rlaekgp94@gmail.com>
2021-03-26 17:19:45 +0900

    section05

--- [1] marshall/js/content.js
+++ [1] marshall/js/content.js
@@ -13,5 +13,21 @@ $(document).ready(function () {
         type: 'progressbar',
       },
     });
-
+    
+    //아티스트 슬라이드
+     var width1=400*$(".stories-slide li").size()+"px";
+    $(".stories-slide").css("width",width1);
+    $(".stories-slide li:last").prependTo(".stories-slide");
+    $(".stories-slide").css("margin-left","-400px");
+    
+    
 });
+
+    function stories(){
+    $(".stories-slide")
+        .animate({marginLeft:parseInt($(".stories-slide").css("margin-left"))-400+"px"},"slow",function(){
+     $(".stories-slide li:first").appendTo(".stories-slide");
+        $(".stories-slide").css("margin-left","-400px");
+    });
+}
+setInterval("stories()",4000);
