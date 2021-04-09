ddb96a22b3d5390a59a83614b864f812a0ae00e4
dahye-kim <rlaekgp94@gmail.com>
2021-04-10 04:11:02 +0900

    last

--- [1] marshall/js/content.js
+++ [1] marshall/js/content.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-
-
+    
     var swiper = new Swiper('.swiper-section02-banner', {
         loop: true,
         effect: 'fade',
@@ -9,25 +8,43 @@ $(document).ready(function () {
             disableOnInteraction: false,
         },
         pagination: {
-        el: '.swiper-pagination',
-        type: 'progressbar',
-      },
+            el: '.swiper-pagination',
+            type: 'progressbar',
+        },
     });
-    
-    //아티스트 슬라이드
-     var width1=400*$(".stories-slide li").size()+"px";
-    $(".stories-slide").css("width",width1);
-    $(".stories-slide li:last").prependTo(".stories-slide");
-    $(".stories-slide").css("margin-left","-400px");
-    
-    
+
+
 });
 
-    function stories(){
+
+//stories slide
+var width1 = 400 * $(".stories-slide li").size() + "px";
+$(".stories-slide").css("width", width1);
+$(".stories-slide li:last").prependTo(".stories-slide");
+$(".stories-slide").css("margin-left", "-400px");
+
+
+
+function stories() {
     $(".stories-slide")
-        .animate({marginLeft:parseInt($(".stories-slide").css("margin-left"))-400+"px"},"slow",function(){
-     $(".stories-slide li:first").appendTo(".stories-slide");
-        $(".stories-slide").css("margin-left","-400px");
-    });
+        .animate({
+            marginLeft: parseInt($(".stories-slide").css("margin-left")) - 400 + "px"
+        }, "slow", function () {
+            $(".stories-slide li:first").appendTo(".stories-slide");
+            $(".stories-slide").css("margin-left", "-400px");
+        });
 }
-setInterval("stories()",4000);
+setInterval("stories()", 5000);
+
+
+
+    
+//    section01
+
+$(document).on('scroll',function(){
+    $('.text-animation01').css("left", Math.max(1000 - 0.7*window.scrollY) + "px");
+})
+
+$(document).on('scroll',function(){
+    $('.text-animation03').css("right", Math.max(1000 - 0.7*window.scrollY) + "px");
+})
